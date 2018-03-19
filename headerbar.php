8ee7de3013b2d90122cfc890e3ae59ff2cd0a644
omegaultron <nageshgt70@gmail.com>
2018-03-19 18:35:02 +0530

    Add files via upload
    navbar code (changes required)

---
+++ headerbar.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="col-sm-8">
+        <div class="row">
+          <div class="col-sm-7">
+            <a class="navbar-brand" href="#">#general</a>
+
+
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="col-sm-7">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">
+                    <span class="glyphicon glyphicon-star-empty"></span>
+                  </a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    <span class="glyphicon glyphicon-user"></span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    <span class="glyphicon glyphicon-pushpin"></span>
+                  </a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    <span class="glyphicon glyphicon-envelope"></span>
+                  </a>
+                </li>
+            </ul>
+
+
+            </div>
+           <div class="col-sm-5"></div>
+          </div>
+        </div>
+      </div>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="row">
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            </form>
+          </div>
+        </div>
+    </nav>
+  </body>
+</html>
