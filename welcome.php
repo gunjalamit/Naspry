80d689a404b175a6be9b2c50063b2bf2cb2c48dc
root <root@localhost.localdomain>
2018-03-21 18:14:52 +0530

    css file links inserted

--- welcome.php
+++ welcome.php
@@ -5,7 +5,7 @@
     <title>Naspry</title>
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-
+    <link href="assets/css/welcomepage.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="assets/css/main.css" rel="stylesheet">
   </head>
@@ -48,7 +48,7 @@
           <p class="welcome_header_para">When your team needs to kick off a project, hire a new employee, deploy some code, review a sales contract, finalize next year's budget, measure an A/B test, plan your next office opening, and more, Slack has you covered.</p>
           <a href="#" class="btn btn-primary btn-lg" role="button"><label class="btn_style">GET STARTED</label></a>
 
-          <p class="welcome_signin_link">Already using Naspry? <a href="/SignIn.php">Sign in</a>.</p>
+          <p class="welcome_signin_link">Already using Naspry? <a href="/lonar2-master/SignIn.php">Sign in</a>.</p>
         </div>
       </div>
 
