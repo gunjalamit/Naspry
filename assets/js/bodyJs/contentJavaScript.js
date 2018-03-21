ba3fd925c4eb17699488114834d8fad926c2686b
root <root@localhost.localdomain>
2018-03-21 17:55:23 +0530

    empty message bug fixed

--- assets/js/bodyJs/contentJavaScript.js
+++ assets/js/bodyJs/contentJavaScript.js
@@ -13,8 +13,9 @@ function showMsg(event) {
       period = " PM";
       hours -= 12;
     }
-    document.getElementById('msgTable').innerHTML += "<tr><td rowspan=2>" +
+    if(msg != "" && msg != null)
+    {document.getElementById('msgTable').innerHTML += "<tr><td rowspan=2>" +
       "<img src='assets/images/slack-user.png' class='rcorner' height='30' width='30'></td><td class='userName'>Naspry<small style='color:#9E9E9E;font-size:12px;padding-left:4px;'>" + hours + ":" + minutes + period + "</small> &nbsp;</td></tr>" + "<tr id='msgRow'><td class='td1'>" + msg + "</td></tr>";
-    return false;
+    return false;}
   }
 }
