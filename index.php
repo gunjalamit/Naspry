99fa7e8319f87e942cde2fde1ada67a0a2b05a22
gunjalamit <amit.gunjal@outlook.com>
2018-03-22 11:35:11 +0530

    minor bugs fixed.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Bootstrap Kickstarter Template</title>
+    <title>#general | team-gsm</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
