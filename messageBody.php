18ba72c01a32125907300932a55ae265fe48b94c
gunjalamit <amit.gunjal@outlook.com>
2018-03-24 15:55:02 +0530

    message date added

--- messageBody.php
+++ messageBody.php
@@ -1,82 +1,84 @@
-
-        <!-- Div Content edit here -->
-                    <!--content starts-->
-<html>
-<head><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-</head>
-<body>
 
-<style>
-.hide{
-	display:none;
-}
-.grey{
-  color:grey;
-}
-.black{
-  color:black;
-  font-weight: bold;
-}
-</style>
+    <head>
 
-<script>
-$(function(){
-    $('[rel="popover"]').popover({
-        html: true,
-        content: function () {
-            var clone = $($(this).data('popover-content')).clone(true).removeClass('hide');
-            return clone;
-        }
-    }).click(function(e) {
-        e.preventDefault();
-    });
-});
-</script>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-<div id="myPopover" class="hide">
-  <p class="grey">New code or text snippet-----------------</p>
-  <a href="#" class="black">Code or text Snippet</a><br>
-  <a href="#" class="black">Post</a><br>
-  <p class="grey">Files from your computer-----------------</p>
-  <input type="file" value="Your Computer">
-</div>
-</head>
-<body>
-          <div id="content" style="margin-left: 242px;">
+        <title>#general | team-gsm</title>
 
-              <div id="msgDiv1" style="margin-top:60px;margin-bottom:60px">
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-                <h6 class="" style="font-weight: bolder;padding-left:500px;padding-right:600px" id="msgTimeBar">Today</h6>
-                        <script>
-                              var date=new Date();
-                              var day=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-                              var month=['January','February','March','April','May','June','July','August','September','October','November','December'];
-                              var d=date.getDate();
-                              var ext=d%10;
-                              var ext1="th";
-                                     if(ext===1)
-                                     {
-                                       ext1="st";
-                                     }
-                                     else if (ext===2) {
-                                       ext1=="nd";
-                                     }
-                                     else if (ext===3) {
-                                       ext1=="rd";
-                                     }
-                              document.getElementById("msgTimeBar").innerHTML=day[date.getDay()]+","+month[date.getMonth()]+' '+d+ext1;
+        <!-- Custom styles for this template -->
+        <link href="assets/css/main.css" rel="stylesheet">
+        <link href="assets/css/style.css" rel="stylesheet">
+        <link href="assets/css/corners.css" rel="stylesheet">
+        <link href="assets/css/right-side-menu.css" rel="stylesheet">
+        <link href="assets/css/bell.css" rel="stylesheet">
+        <link href="assets/css/channel.css" rel="stylesheet">
+        <link href="assets/css/bodytext.css" rel="stylesheet">
+        <link href="assets/css/divpadding.css" rel="stylesheet">
+        <link href="assets/css/dropdown.css" rel="stylesheet">
+        <link href="assets/css/tooltip.css" rel="stylesheet">
+        <link href="assets/css/option-menu.css" rel="stylesheet">
+        <link href="assets/css/msgDisplay.css" rel="stylesheet">
+        <link href="assets/css/notification_dropdown.css" rel="stylesheet">
+        <link href="assets/css/scrollbar.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <script src="assets/js/bodyJs/contentJavaScript.js"></script>
+        <script src="assets/js/dropdowm.js"></script>
+        <script src="assets/js/dropjs.js"></script>
+        <script src="assets/js/headerclick.js"></script>
+        <script src="assets/js/right-side-menu.js"></script>
+        <script src="assets/js/bellfunction.js"></script>
+        <script src="assets/js/belltooltip.js"></script>
+        <script src="assets/js/popupmsg.js"></script>
+        <!--script src="assets/js/channels.js"></script-->
+        <script src="assets/js/dropdown.js"></script>
+        <script src="assets/js/option-menu.js"></script>
+
+          <!-- Div Content edit here -->
+                    <!--content starts-->
+
+
+                    <div id="content" style="margin-left: 0;">
+<!--
+                      <div id="attach-btn-popup" class="popover popover-default">
+                          <a href="#">Code or text Snippet</a><br>
+                          <a href="#">Post</a><br>
+                          <input type="file" value="Your Computer">
+                     </div> -->
+
+
+                    <div id="msgDiv" style="margin-top:60px;margin-bottom:60px">
+                      <h6 style="padding-left:450px;padding-right:400px" id="msgTimeBar">Today</h6>
+                     <script>
+                             var date=new Date();
+                             var day=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+                             var month=['January','February','March','April','May','June','July','Augest','September','October','November','December'];
+                             var d=date.getDate();
+                             var ext=d%10;
+                             var ext1="th";
+                             if(ext===1)
+                             {
+                               ext1="st";
+                             }
+                             else if (ext===2) {
+                               ext1=="nd";
+                             }
+                             else if (ext===3) {
+                               ext1=="rd";
+                             }
+                             document.getElementById("msgTimeBar").innerHTML=day[date.getDay()]+","+month[date.getMonth()]+' '+d+ext1;
 
                      </script>
-
                         <!--msg table starts-->
                         <table border="0" cellspacing="5" cellpadding="2" id="msgTable" class="tablecorners">
 
@@ -86,12 +88,11 @@ $(function(){
                     <div class="fixed">
                         <table>
                             <tr>
-                                <td><button id="attach-btn-id" class="attach-btn" value="+" rel="popover" data-placement="top" data-popover-content="#myPopover">+</button></td>
+                                <td><input id="attach-btn-id" class="attach-btn" type="button" value="+" data-toggle="popover-x" data-target="#attach-btn-popup" data-placement="top"></td>
                                 <td style="width:1300px"><input type="text" id="message" onkeypress="return showMsg(event)" placeholder="Message #general"></td>
                             </tr>
                         </table>
                     </div>
                   </div>
-          </div>
 
                     <!--content ends-->
